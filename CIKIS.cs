06534611ae4e05fbd0ede7e7a7f6c1047ca99d7b
Mertzehir06 <74835829+Mertzehir06@users.noreply.github.com>
2021-03-21 23:09:11 +0300

    Add files via upload

---
+++ CIKIS.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CIKIS : MonoBehaviour
+{
+    public void cıkıs()
+    {
+        Application.Quit();
+    }
+}
---
+++ endd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endd : MonoBehaviour
+{
+    public GameObject Endscrn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "CollectibleObj")
+        {
+            Endscrn.gameObject.SetActive(true);
+        }
+    }
+
+
+
+}
